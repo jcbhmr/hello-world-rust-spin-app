f345610ec84a511ff17cbf1e04bb1e694863cfda
Jacob Hummer <jcbhmr@outlook.com>
2023-12-29 13:38:08 -0600

    back to default starter

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,7 @@ use spin_sdk::http_component;
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_hello_world_rust_spin_app(req: Request) -> anyhow::Result<impl IntoResponse> {
-    let url = req
-        .header("spin-full-url")
-        .map_or("", |v| v.as_str().unwrap_or_default());
-    println!("Handling request to {:?}", url);
+    println!("Handling request to {:?}", req.header("spin-full-url"));
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
